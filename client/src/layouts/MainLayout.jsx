5a9b71584ab3342bad3d25ddaa9d22102d5a7e1f
spicartsdeployment <spicartsdeployments@gmail.com>
2025-06-02 15:08:54 +0530

    Update MainLayout.jsx

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/navbar/navbar";
+import Navbar from "../components/navbar/Navbar";
 import ScrollObserver from "../components/common-components/intersectObserver/IntersectObserver";
 
 const MainLayout = () => {
